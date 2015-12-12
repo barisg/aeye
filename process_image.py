e7de1dd17046a71bac145bd28699d2bfeed3f608
Barisg <baris-git@gmail.com>
2015-12-12 13:30:03 -0800

    start image processing edits

--- process_image.py
+++ process_image.py
@@ -2,7 +2,10 @@ import sklearn
 import numpy
 import urllib
 import urllib2
+import Image
 
 def recognize_image(url):
     # return '42'
-    return url
+    img = Image.open(urllib2.open(url).read())
+    
+    return img.size
